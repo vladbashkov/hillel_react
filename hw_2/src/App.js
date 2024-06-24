bd354dab04e6f97b5bbff1efc57c50028a78820b
vladbashkov <vlad@cgspark.com>
2024-06-24 21:52:58 +0300

    Different data has different console log output(and maybe render)

--- hw_2/src/App.js
+++ hw_2/src/App.js
@@ -1,28 +1,31 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+// import axios from "axios";
 
 import "./App.scss";
 import { STATUS_TO_DO, STATUS_IN_PROGRESS, STATUS_DONE } from "./constants/Constants";
-// import { tasks as taskData } from "./data/mockData";
+import { tasks as taskData } from "./data/mockData";
 import { handleChangeStatus, handleDeleteTask } from "./utils/tasksUtils";
 import Column from "./components/Columns/Columns";
 
 function App() {
-	const [tasks, setTasks] = useState([]);
+	// const [tasks, setTasks] = useState([]);
+	const [tasks, setTasks] = useState(taskData);
 	console.log("useEffect 0");
 
 	useEffect(() => {
-		console.log("useEffect 1");
-		axios
-			.get("https://6675570ea8d2b4d072efa0bb.mockapi.io/tasks")
-			.then((response) => {
-				setTasks(response.data);
-				console.log("useEffect 2");
-			})
-			.catch((error) => {
-				console.error("Error fetching data: ", error);
-			});
+		// console.log("useEffect 1");
+		// axios
+		// 	.get("https://6675570ea8d2b4d072efa0bb.mockapi.io/tasks")
+		// 	.then((response) => {
+		// 		setTasks(response.data);
+		// 		console.log("useEffect 2");
+		// 	})
+		// 	.catch((error) => {
+		// 		console.error("Error fetching data: ", error);
+		// 	});
 	}, []);
+
+	useEffect(() => {}, [tasks]);
 
 	const changeStatusHandler = handleChangeStatus(tasks, setTasks);
 	const deleteTaskHandler = handleDeleteTask(tasks, setTasks);
