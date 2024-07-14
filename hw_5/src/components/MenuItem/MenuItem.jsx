687de5e636124a5abd09d9e926e6f38857c36f8d
vladbashkov <vlad@cgspark.com>
2024-07-14 14:29:35 +0300

    HW_7 ready for qa

--- hw_5/src/components/MenuItem/MenuItem.jsx
+++ hw_5/src/components/MenuItem/MenuItem.jsx
@@ -1,23 +1,64 @@
-import { Soldout } from "../../constants/Constants";
+import { useState } from "react";
+import Button from "../Button/Button";
 
 const MenuItem = ({ item }) => {
+	const [quantity, setQuantity] = useState(0);
+
+	const handleAddToCart = () => {
+		setQuantity(1);
+	};
+
+	const handleIncrement = () => {
+		setQuantity(quantity + 1);
+	};
+
+	const handleDecrement = () => {
+		if (quantity > 1) {
+			setQuantity(quantity - 1);
+		}
+	};
+
+	const handleDelete = () => {
+		setQuantity(0);
+	};
+
 	return (
 		<li className={`pizza ${item.soldOut ? "soldout" : ""}`} id={item.id}>
 			<img src={item.imageUrl} className="pizza__image" alt={item.name} />
 			<div className="pizza__info">
 				<p className="pizza__name">{item.name}</p>
-				{/* <div className="pizza__ingredients">
+				<p className="pizza__ingredients">
 					{item.ingredients.map((ing, index) => (
-						<p key={index} className="pizza__ingredient">
-							{item.ingredients.length - 1 == index ? `${ing}` : `${ing},`}
-						</p>
+						<span key={index}>
+							{ing}
+							{index < item.ingredients.length - 1 && ", "}
+						</span>
 					))}
-					<p className="pizza__ingredient">{item.ingredients.join(", ")}</p>
-				</div> */}
-				<p className="pizza__ingredients">{item.ingredients.join(", ")}</p>
+				</p>
 				<div className="pizza__actions">
-					<p className="pizza__price">{item.soldOut ? Soldout : `€${item.unitPrice}.00`}</p>
-					{!item.soldOut && <button className="button">Add to cart</button>}
+					<p className="pizza__price">€{item.unitPrice}</p>
+					{!item.soldOut && (
+						<>
+							{quantity === 0 ? (
+								<Button onClick={handleAddToCart} className="button">
+									Add to cart
+								</Button>
+							) : (
+								<div className="buttons-container">
+									<Button onClick={handleDecrement} className={quantity === 1 ? "button single" : "button"}>
+										-
+									</Button>
+									<span>{quantity}</span>
+									<Button onClick={handleIncrement} className="button">
+										+
+									</Button>
+									<Button onClick={handleDelete} className="button">
+										DELETE
+									</Button>
+								</div>
+							)}
+						</>
+					)}
 				</div>
 			</div>
 		</li>
